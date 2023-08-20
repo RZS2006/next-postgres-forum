787d84c8d7420fa831769212fd26a1b840638997
Raiyan Shamim <razash139@gmail.com>
2023-08-20 19:24:14 +0300

    prettier

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,7 @@
 export default function Home() {
   return (
     <main className="min-h-screen">
-      <h1 className="text-lg font-semibold">Hello</h1>
+      <h1 className="hover:text-green-600 text-lg font-semibold ">Hello</h1>
     </main>
-  )
+  );
 }
